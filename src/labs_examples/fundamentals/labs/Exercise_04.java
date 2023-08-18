cf7882f834a164828d1158553111c5469ac7bf4b
hesc23 <hendrik.schuster@consileon.de>
2023-08-18 12:30:26 +0200

    completed exercises 4 & 5

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,7 +16,23 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        boolean bool = false;
+        char c = 'U';
+        byte b = 99;
+        short s = 25643;
+        long l = 251000123213213l;
+        double d = 123.21323123;
+        float f = 1.324f;
+        int y = 123;
 
+        System.out.println(bool);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(y);
     }
 
 }
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,15 +14,21 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int i = str.length();
+        System.out.println(i);
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean equal = str.equals(str2);
+        System.out.println(equal);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String new_string = str + str2;
+        System.out.println(new_string);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
-
+        System.out.println(new_string.indexOf("o"));
     }
 
 
