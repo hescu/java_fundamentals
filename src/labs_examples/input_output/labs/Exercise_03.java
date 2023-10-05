08a411a4dfc82975b484bcd6d13de89bc4511e5b
hesc23 <hendrik.schuster@consileon.de>
2023-10-05 18:01:08 +0200

    WIP: began Exercise_03

--- src/labs_examples/input_output/labs/Exercise_03.java
+++ src/labs_examples/input_output/labs/Exercise_03.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 3: variety
  *
@@ -9,3 +11,30 @@ package labs_examples.input_output.labs;
  *    4) Demonstrate the use of the DataInputStream and DataOutputStream
  *
  */
+
+class Exercise_03 {
+    public static void main(String[] args) {
+        byte[] array = {11, 22, 33, 44, 55, 127};
+
+        try (BufferedInputStream input = new BufferedInputStream(new ByteArrayInputStream(array), 3)) {
+            System.out.print("Bytes read: \n");
+
+            for (int i = 0; i < array.length; i++) {
+                int data = input.read();
+                System.out.println(data);
+            }
+        } catch (IOException exc) {
+            exc.getStackTrace();
+        }
+    }
+}
+
+class DataStream {
+    public static void main(String[] args) {
+        try (BufferedReader input = new BufferedReader(new (System.in))) {
+            String sentence = input.readLine();
+        } catch (IOException exc) {
+            exc.getStackTrace();
+        }
+    }
+}
