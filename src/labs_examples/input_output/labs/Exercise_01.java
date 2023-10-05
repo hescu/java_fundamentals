9c9b498a956120b93f6a438489a52a124ca1c00d
hesc23 <hendrik.schuster@consileon.de>
2023-10-05 14:13:03 +0200

    I/O labs exercises 1 and 2 done

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -11,8 +13,18 @@ package labs_examples.input_output.labs;
 
 class Example {
     public static void main(String[] args) {
+        String filePath = "src/labs_examples/input_output/labs/files/text_file_to_read.txt";
+        String newFilePath = "src/labs_examples/input_output/files/labs/text_file_to_write.txt";
 
-
+        try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(filePath), 5);
+                FileOutputStream outputStream = new FileOutputStream(newFilePath)) {
+            int i;
+            while ((i = inputStream.read()) != -1) {
+                outputStream.write((char) i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- src/labs_examples/input_output/labs/Exercise_02.java
+++ src/labs_examples/input_output/labs/Exercise_02.java
@@ -1,5 +1,10 @@
 package labs_examples.input_output.labs;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Input/Output Exercise 2: File encryption
  *
@@ -12,3 +17,23 @@ package labs_examples.input_output.labs;
  *
  */
 
+class Exercise_02 {
+    public static void main(String[] args) {
+        String filePath = "src/labs_examples/input_output/labs/files/message.txt";
+        String newFilePath = "src/labs_examples/input_output/labs/files/encrypted_message.txt";
+
+        try (BufferedReader inputReader = new BufferedReader(new FileReader(filePath));
+                FileWriter outputWriter = new FileWriter(newFilePath)) {
+            int i;
+            while ((i = inputReader.read()) != -1) {
+                if ((char) i == 'a') {
+                    outputWriter.write('x');
+                } else {
+                    outputWriter.write(i+1);
+                }
+            }
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+    }
+}
