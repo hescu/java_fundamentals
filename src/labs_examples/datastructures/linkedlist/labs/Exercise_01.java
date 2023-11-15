eea15e8f6268f9a8cd0a7dd1e1653e37c366461c
hesc23 <hendrik.schuster@consileon.de>
2023-11-15 14:12:19 +0100

    linkedList exercise 01 done

--- src/labs_examples/datastructures/linkedlist/labs/Exercise_01.java
+++ src/labs_examples/datastructures/linkedlist/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.datastructures.linkedlist.labs;
 
+import java.util.*;
+
 /**
  *      LinkedLists - Exercise_01
  *
@@ -21,3 +23,42 @@ package labs_examples.datastructures.linkedlist.labs;
  *      14) listIterator()
  *      15) clear()
  */
+
+public class Exercise_01 {
+    public static void main(String[] args) {
+        LinkedList<Integer> numbers = new LinkedList<>();
+
+        numbers.add(213213);
+
+        Collection<Integer> collectionOfNumbers = new ArrayList<>();
+        collectionOfNumbers.add(42123);
+        collectionOfNumbers.add(4645);
+        collectionOfNumbers.add(8979841);
+        collectionOfNumbers.add(21);
+        numbers.addAll(1, collectionOfNumbers);
+
+        numbers.addFirst(1);
+
+        numbers.addLast(999);
+
+        System.out.println("First: " + numbers.getFirst());
+        System.out.println("Last: " + numbers.getLast());
+        System.out.println("Index 4: " + numbers.get(4));
+
+        numbers.set(0, 888);
+        numbers.push(42);
+        System.out.println("Pop: " + numbers.pop());
+        System.out.println("Remove at index 3: " + numbers.remove(3));
+        System.out.println("Does it contain 999? " + numbers.contains(999));
+
+        ListIterator<Integer> listIterator = numbers.listIterator(1);
+        System.out.println("ListIterator:");
+        while (listIterator.hasNext()) {
+            System.out.println(listIterator.next());
+        }
+
+        numbers.clear();
+        System.out.println("Size after clear(): " + numbers.size());
+
+    }
+}
