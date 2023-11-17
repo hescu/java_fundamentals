746e055b966b703d62facd3d3a145e0694fccbc6
Hendrik Schuster <hendrik.schuster@consileon.de>
2023-11-17 10:53:12 +0100

    stack exercise_01 done

--- src/labs_examples/datastructures/stack/labs/Exercise_01.java
+++ src/labs_examples/datastructures/stack/labs/Exercise_01.java
@@ -1,8 +1,24 @@
 package labs_examples.datastructures.stack.labs;
 
+import java.util.Stack;
+
 /**
  *      Demonstrate your mastery of Java's built-in Stack class by completing the following:
  *
  *      1) instantiate a new Stack
  *      2) demonstrate the use of EVERY method in the Stack class - there are 5
  */
+
+public class Exercise_01 {
+    public static void main(String[] args) {
+        Stack<String> myStack = new Stack<>();
+
+        myStack.push("Jambalaya");
+        myStack.push("Gumbo");
+        myStack.push("Paella");
+        System.out.println(myStack.peek());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.search("Paella"));
+        System.out.println(myStack.empty());
+    }
+}
